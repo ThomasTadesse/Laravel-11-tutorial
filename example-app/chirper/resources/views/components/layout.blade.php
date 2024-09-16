1b6e9a9e62eeb7f24ea3b7dd51d8c90ab579a4a5
ThomasTadesse <328704@student.mboutrecht.nl>
2024-09-16 11:19:56 +0200

    Refactor views: Update user profile information in layout component

--- example-app/chirper/resources/views/components/layout.blade.php
+++ example-app/chirper/resources/views/components/layout.blade.php
@@ -88,8 +88,8 @@
             <img class="h-10 w-10 rounded-full" src="https://m.media-amazon.com/images/M/MV5BZjZhNTI3MWMtODYyNS00OTQ3LWFlYTktYTQwZjRhMWQ4Zjc0XkEyXkFqcGdeQWRpZWdtb25n._V1_QL75_UX500_CR0,0,500,281_.jpg" alt="">
           </div>
           <div class="ml-3">
-            <div class="text-base font-medium leading-none text-white">Tom Cook</div>
-            <div class="text-sm font-medium leading-none text-gray-400">tom@example.com</div>
+            <div class="text-base font-medium leading-none text-white">John Doe</div>
+            <div class="text-sm font-medium leading-none text-gray-400">J.Doe@hotmail.com</div>
           </div>
           <button type="button" class="relative ml-auto flex-shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
             <span class="absolute -inset-1.5"></span>
