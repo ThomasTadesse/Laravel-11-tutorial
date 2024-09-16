ea40f77ee434abc8f12b03785b266e34fe1f668f
ThomasTadesse <328704@student.mboutrecht.nl>
2024-09-16 10:38:51 +0200

    3 main paginas bijgewerkt

--- example-app/chirper/routes/web.php
+++ example-app/chirper/routes/web.php
@@ -4,13 +4,19 @@ use App\Http\Controllers\ChirpController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/home', function () {
+    return view('home');
 });
 
 Route::get('/about', function () {
     return view('about');
 });
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
