07b24bf892871d103114e94f62fe49b8e2a752e6
ThomasTadesse <328704@student.mboutrecht.nl>
2024-09-16 11:18:55 +0200

    Refactor views: Update layout components and replace company logos

--- example-app/chirper/resources/views/components/layout.blade.php
+++ example-app/chirper/resources/views/components/layout.blade.php
@@ -21,7 +21,7 @@
       <div class="flex h-16 items-center justify-between">
         <div class="flex items-center">
           <div class="flex-shrink-0">
-            <img class="h-8 w-8" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company">
+            <img class="h-8 w-8" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSVWvmq6kUZXxJGTlSYOoqyqHj-sAcpJLseA&s" alt="Your Company">
           </div>
           <div class="hidden md:block">
             <div class="ml-10 flex items-baseline space-x-4">
@@ -48,7 +48,7 @@
                 <button type="button" class="relative flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
                   <span class="absolute -inset-1.5"></span>
                   <span class="sr-only">Open user menu</span>
-                  <img class="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
+                  <img class="h-8 w-8 rounded-full" src="https://m.media-amazon.com/images/M/MV5BZjZhNTI3MWMtODYyNS00OTQ3LWFlYTktYTQwZjRhMWQ4Zjc0XkEyXkFqcGdeQWRpZWdtb25n._V1_QL75_UX500_CR0,0,500,281_.jpg" alt="">
                 </button>
               </div>
 
@@ -85,7 +85,7 @@
       <div class="border-t border-gray-700 pb-3 pt-4">
         <div class="flex items-center px-5">
           <div class="flex-shrink-0">
-            <img class="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
+            <img class="h-10 w-10 rounded-full" src="https://m.media-amazon.com/images/M/MV5BZjZhNTI3MWMtODYyNS00OTQ3LWFlYTktYTQwZjRhMWQ4Zjc0XkEyXkFqcGdeQWRpZWdtb25n._V1_QL75_UX500_CR0,0,500,281_.jpg" alt="">
           </div>
           <div class="ml-3">
             <div class="text-base font-medium leading-none text-white">Tom Cook</div>
