c8b2c90b8e0afa7e2efeafd77b1d3051cdfbeea5
ThomasTadesse <328704@student.mboutrecht.nl>
2024-09-23 10:31:58 +0200

    Refactor views: Update layout components to use custom navigation links and add button option for nav-link

--- example-app/chirper/resources/views/components/nav-link.blade.php
+++ example-app/chirper/resources/views/components/nav-link.blade.php
@@ -1,6 +1,15 @@
-@props(['active' => false])
+@props(['active' => false, 'type' => 'a'])
 
-<a class="{{ $active ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }} px-3 py-2 rounded-md text-sm font-medium" 
+
+<?php if ($type =='a') : ?>
+  <a class="{{ $active ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }} px-3 py-2 rounded-md text-sm font-medium" 
+  aria-current="{{ $active ? 'page' : 'false' }}"
+  {{ $attributes }}
+  >{{ $slot }}</a>
+
+<?php else :?>
+<button class="{{ $active ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }} px-3 py-2 rounded-md text-sm font-medium" 
 	aria-current="{{ $active ? 'page' : 'false' }}"
 	{{ $attributes }}>
-  {{ $slot }}</a>
+  {{ $slot }}</button>
+<?php endif; ?>
