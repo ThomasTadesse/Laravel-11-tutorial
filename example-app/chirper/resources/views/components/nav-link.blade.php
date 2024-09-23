49c385469f4427cd193e6cd3e001fb862da31b36
ThomasTadesse <328704@student.mboutrecht.nl>
2024-09-23 10:23:46 +0200

    nav box fix

--- example-app/chirper/resources/views/components/nav-link.blade.php
+++ example-app/chirper/resources/views/components/nav-link.blade.php
@@ -1,11 +1,6 @@
-<a class="{{ request()->is('home') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }} rounded-md px-3 py-2 text-sm font-medium" 
-   aria-current="{{ request()->is('home') ? 'page' : 'false' }}">
-	{{ $slot }}
-</a>
+@props(['active' => false])
 
-{{-- Debugging output --}}
-@if(request()->is('home'))
-	<p>Current URL matches 'home'</p>
-@else
-	<p>Current URL does not match 'home'</p>
-@endif
+<a class="{{ $active ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }} px-3 py-2 rounded-md text-sm font-medium" 
+	aria-current="{{ $active ? 'page' : 'false' }}"
+	{{ $attributes }}>
+  {{ $slot }}</a>
