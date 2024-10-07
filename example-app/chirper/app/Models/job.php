3af96280eca013f9b3b9807bb09871ba590d0c1d
ThomasTadesse <328704@student.mboutrecht.nl>
2024-10-07 10:42:46 +0200

    job model to use Eloquent Model and specify table name

--- example-app/chirper/app/Models/job.php
+++ example-app/chirper/app/Models/job.php
@@ -12,4 +12,9 @@ class Job extends Model {
   protected $table = 'job_listings';
 
   protected $fillable = ['title', 'description'];
+
+  public function employer() {
+    return $this->belongsTo(Employer::class);
+  }
+
 }
