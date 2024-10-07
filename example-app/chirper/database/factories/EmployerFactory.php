f6d16844f25ee567d4eaad0126d7064c486b8e0a
ThomasTadesse <328704@student.mboutrecht.nl>
2024-10-07 10:40:40 +0200

    10: Model Factories compleet

--- example-app/chirper/database/factories/EmployerFactory.php
+++ example-app/chirper/database/factories/EmployerFactory.php
@@ -5,9 +5,9 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Job>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Employer>
  */
-class JobFactory extends Factory
+class EmployerFactory extends Factory
 {
     /**
      * Define the model's default state.
@@ -17,9 +17,7 @@ class JobFactory extends Factory
     public function definition(): array
     {
         return [
-            'title' => fake()->sjobTitle(),
-            'employer_id' => Employer::factory(),
-            'description' => fake()->jobDescription()
+            'name' => fake()->company()
         ];
     }
 }
