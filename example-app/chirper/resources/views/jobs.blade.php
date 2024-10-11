9e0fb7fab765adcc8571b007480dba5b111e61dc
ThomasTadesse <162581606+ThomasTadesse@users.noreply.github.com>
2024-10-11 13:06:57 +0200

    Update jobs.blade.php

--- example-app/chirper/resources/views/jobs.blade.php
+++ example-app/chirper/resources/views/jobs.blade.php
@@ -3,12 +3,14 @@
         Job Listings
     </x-slot:heading>
 
-    <ul>
+    <div class="space-y-6">
     @foreach ($jobs as $job)
-        <li>
-            <a href="/jobs/ {{ $job['id'] }}" class="text-green-500 hover:underline">
-            <strong>{{ $job['title'] }}:</strong> {{ $job['description'] }}</li>
-            </a>
+            <a href="/jobs/ {{ $job['id'] }}" class="block px-4 py-6 border border-gray-200 rounded-lg">
+            <div>{{ $job->employer->name }}</div>
+            <div>
+            <strong>{{ $job['title'] }}:</strong> {{ $job['description'] }}
+            </div>    
+        </a>
     @endforeach
-    </ul>
+    </div>
 </x-layout>
