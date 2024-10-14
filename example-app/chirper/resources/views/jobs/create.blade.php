e40324e5b9dcef9eae33a912e18d13da55bb16b3
ThomasTadesse <328704@student.mboutrecht.nl>
2024-10-14 11:29:08 +0200

    job form layout and field names

--- example-app/chirper/resources/views/jobs/create.blade.php
+++ example-app/chirper/resources/views/jobs/create.blade.php
@@ -25,23 +25,25 @@
 
       <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
         <div class="sm:col-span-4">
-          <label for="username" class="block text-sm font-medium leading-6 text-gray-900">Username</label>
+          <label for="title" class="block text-sm font-medium leading-6 text-gray-900">Title</label>
           <div class="mt-2">
             <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-              <span class="flex select-none items-center pl-3 text-gray-500 sm:text-sm">workcation.com/</span>
-              <input type="text" name="username" id="username" autocomplete="username" class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="janesmith">
+              <input type="text" name="title" id="title" class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="Vakkenvuller">
             </div>
           </div>
         </div>
 
-        <div class="col-span-full">
-          <label for="about" class="block text-sm font-medium leading-6 text-gray-900">About</label>
+    
+        <div class="sm:col-span-4">
+        <label for="description" class="block text-sm font-medium leading-6 text-gray-900">Description</label>
           <div class="mt-2">
-            <textarea id="about" name="about" rows="3" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
+            <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+              <input type="text" name="description" id="description" class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="Vult de vakken.">
+            </div>
           </div>
-          <p class="mt-3 text-sm leading-6 text-gray-600">Write a few sentences about yourself.</p>
-        </div>
 
+
+        
       
 
   <div class="mt-6 flex items-center justify-end gap-x-6">
