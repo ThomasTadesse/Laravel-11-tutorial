8d82b7adfb280abe2727acf3078c8d9f06369b5f
ThomasTadesse <328704@student.mboutrecht.nl>
2024-10-14 11:32:58 +0200

    job creation form layout and field names

--- example-app/chirper/resources/views/jobs/create.blade.php
+++ example-app/chirper/resources/views/jobs/create.blade.php
@@ -3,25 +3,12 @@
         Create Job
     </x-slot:heading>
     
-   <!--
-  This example requires some changes to your config:
-  
-  ```
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/forms'),
-    ],
-  }
-  ```
--->
-<form>
+ 
+<form method="POST" action="/jobs">
   <div class="space-y-12">
     <div class="border-b border-gray-900/10 pb-12">
-      <h2 class="text-base font-semibold leading-7 text-gray-900">Profile</h2>
-      <p class="mt-1 text-sm leading-6 text-gray-600">This information will be displayed publicly so be careful what you share.</p>
+      <h2 class="text-base font-semibold leading-7 text-gray-900">Create a new job</h2>
+      <p class="mt-1 text-sm leading-6 text-gray-600">This information will be required, So be careful what you put out there.</p>
 
       <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
         <div class="sm:col-span-4">
