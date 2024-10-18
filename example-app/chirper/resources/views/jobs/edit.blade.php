5753576330b77b37f7440cbce2694678c1f28a7c
ThomasTadesse <328704@student.mboutrecht.nl>
2024-10-18 13:33:43 +0200

    job creation form inputs for better readability

--- example-app/chirper/resources/views/jobs/edit.blade.php
+++ example-app/chirper/resources/views/jobs/edit.blade.php
@@ -15,7 +15,12 @@
           <label for="title" class="block text-sm font-medium leading-6 text-gray-900">Title</label>
           <div class="mt-2">
             <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-              <input type="text" name="title" id="title" class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="Manager" required>
+              <input type="text" 
+                     name="title" 
+                     id="title" 
+                     class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" 
+                     placeholder="Manager" 
+                     required>
             </div>
 
             @error('title')
@@ -44,7 +49,12 @@
         <label for="description" class="block text-sm font-medium leading-6 text-gray-900">Description</label>
           <div class="mt-2">
             <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-              <input type="text" name="description" id="description" class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="Onderhandelt mensen." required>
+              <input type="text" 
+                     name="description" 
+                     id="description" 
+                     class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" 
+                     placeholder="Onderhandelt mensen." 
+                     required>
             </div>
 
             @error('description')
