6df6366f87b0296dbe0db8635db1408fdedc4554
ThomasTadesse <328704@student.mboutrecht.nl>
2024-10-18 13:36:45 +0200

    removed unnecessary code

--- example-app/chirper/resources/views/jobs/edit.blade.php
+++ example-app/chirper/resources/views/jobs/edit.blade.php
@@ -29,19 +29,6 @@
             @enderror
 
 
-
-            {{-- 
-            <div class="mt-10">
-            @if($errors->any())
-            <ul>
-                @foreach($errors->all() as $error)
-                <li class="text-red-500 text-xs">{{ $error }}</li>
-                @endforeach
-            </ul>
-            @endif
-            </div>
-            --}}
-
           </div>
         </div>
 
@@ -70,8 +57,16 @@
       
 
   <div class="mt-6 flex items-center justify-end gap-x-6">
-    <button type="button" class="text-sm font-semibold leading-6 text-gray-900">Cancel</button>
-    <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
+    <button type="button" 
+    class="text-sm font-semibold leading-6 text-gray-900">
+    Cancel
+    </button>
+
+    <button type="submit" 
+    class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+    Update
+    </button>
+    
   </div>
 </form>
 
