db869d2a98b442c1c3f728b1c3fcca342770e76b
ThomasTadesse <328704@student.mboutrecht.nl>
2024-10-24 15:16:39 +0200

    JobController to include CRUD methods and pagination for job listings

--- example-app/chirper/app/Http/Controllers/JobController.php
+++ example-app/chirper/app/Http/Controllers/JobController.php
@@ -29,31 +29,72 @@ class JobController extends Controller
 
     public function create()
     {
-        
+        return view('jobs.create');
     }
 
-    public function show()
+    public function show(job $job)
+    // your parameter name should match the wildcard name.
+    // so in this case: {job} should match $job.
+    // easy.
+   
     {
-        
+   
+        return view('jobs.show', [
+            'job' => $job,
+        ]);
     }
 
     public function store()
     {
-        
+        request()->validate([
+            'title' => ['required', 'min:3'],
+            'description' => ['required'],
+        ]);
+    
+        Job::create([
+            'title' => request('title'),
+            'description' => request('description'),
+            'employer_id' => 1
+        ]);
+    
+        return redirect('/jobs');
     }
 
-    public function edit()
+    public function edit(job $job)
     {
-        
+        return view('jobs.edit', [
+            'job' => $job,
+        ]);
     }
 
-    public function update()
+    public function update(job $job)
     {
-        
+           // validation
+    request()->validate([
+        'title' => ['required', 'min:3'],
+        'description' => ['required'],
+    ]);
+
+    // authorization (on hold for now.)
+
+    $job->update([
+        'title' => request('title'),
+        'description' => request('description'),
+    ]);
+
+    return redirect("/jobs/{$job->id}");
+
     }
 
-    public function destroy()
+    public function destroy(job $job)
     {
-        
+        ($job)->delete();
+
+        return redirect('/jobs');   
     }
 }
+
+// keep in mind that get, post, put, patch, delete are all methods that can be used in the routes.
+// so theres no need to use add them at the end of the route.
+// good example: look above.
+// bad example: Route::get('/jobs/{id}/edit', function ($id){
--- example-app/chirper/routes/web.php
+++ example-app/chirper/routes/web.php
@@ -5,7 +5,6 @@ use App\Http\Controllers\ChirpController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Arr;
-use App\Models\Job;
 use App\Http\Controllers\JobController;
 
 Route::get('/home', function () {
@@ -13,87 +12,13 @@ Route::get('/home', function () {
 });
 
 
-// index
 Route::get('/jobs', [JobController::class, 'index']);
-
-
-// create
-Route::get('/jobs/create', function () {
-    return view('jobs.create');
-});
-
-
-// show
-Route::get('/jobs/{job}', function (job $job){
-   // your parameter name should match the wildcard name.
-   // so in this case: {job} should match $job.
-   // easy.
-   
-
-    
-        return view('jobs.show', [
-            'job' => $job,
-        ]);
-});
-
-
-// store
-Route::post('/jobs', function () {
-    request()->validate([
-        'title' => ['required', 'min:3'],
-        'description' => ['required'],
-    ]);
-
-    Job::create([
-        'title' => request('title'),
-        'description' => request('description'),
-        'employer_id' => 1
-    ]);
-
-    return redirect('/jobs');
-});
-
-// Edit
-Route::get('/jobs/{job}/edit', function (job $job){
-    
-        return view('jobs.edit', [
-            'job' => $job,
-        ]);
-});
-
-
-// Update
-Route::patch('/jobs/{job}', function (job $job){
-    // validation
-    request()->validate([
-        'title' => ['required', 'min:3'],
-        'description' => ['required'],
-    ]);
-
-    // authorization (on hold for now.)
-
-    $job->update([
-        'title' => request('title'),
-        'description' => request('description'),
-    ]);
-
-    return redirect("/jobs/{$job->id}");
-
-});
-
-// Destroy
-Route::delete('/jobs/{job}', function (job $job){
-
-    ($job)->delete();
-
-    return redirect('/jobs');   
-
-});
-
-// keep in mind that get, post, put, patch, delete are all methods that can be used in the routes.
-// so theres no need to use add them at the end of the route.
-// good example: look above.
-// bad example: Route::get('/jobs/{id}/edit', function ($id){
+Route::get('/jobs/create', [JobController::class, 'create']);
+Route::get('/jobs/{job}', [JobController::class, 'show']);
+Route::post('/jobs', [JobController::class, 'store']);
+Route::get('/jobs/{job}/edit', [JobController::class, 'edit']);
+Route::patch('/jobs/{job}', [JobController::class, 'update']);
+Route::delete('/jobs/{job}', [JobController::class, 'destroy']);
 
 
 
