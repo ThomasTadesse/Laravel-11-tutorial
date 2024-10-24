fbea156caa9e7f424410cbe36be207a1b118d6e2
ThomasTadesse <328704@student.mboutrecht.nl>
2024-10-24 14:47:16 +0200

    JobController to include all CRUD methods

--- example-app/chirper/app/Http/Controllers/JobController.php
+++ example-app/chirper/app/Http/Controllers/JobController.php
@@ -6,5 +6,38 @@ use Illuminate\Http\Request;
 
 class JobController extends Controller
 {
-    //
+    public function index()
+    {
+        
+    }
+
+    public function create()
+    {
+        
+    }
+
+    public function show()
+    {
+        
+    }
+
+    public function store()
+    {
+        
+    }
+
+    public function edit()
+    {
+        
+    }
+
+    public function update()
+    {
+        
+    }
+
+    public function destroy()
+    {
+        
+    }
 }
