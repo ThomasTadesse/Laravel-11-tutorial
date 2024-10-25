2d16aa03a5ba4748826456b26470ee1b9f41d210
ThomasTadesse <328704@student.mboutrecht.nl>
2024-10-25 14:24:08 +0200

    RegisteredUserController.php for improved user registration and authentication

--- example-app/chirper/app/Http/Controllers/RegisteredUserController.php
+++ example-app/chirper/app/Http/Controllers/RegisteredUserController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rules\Password;
 
 class RegisteredUserController extends Controller
 {
