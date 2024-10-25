1cecb5a0f6c1b217c00411cfde52e4bbdb04e3b7
ThomasTadesse <328704@student.mboutrecht.nl>
2024-10-25 13:45:17 +0200

    login test page

--- example-app/chirper/routes/web.php
+++ example-app/chirper/routes/web.php
@@ -32,6 +32,7 @@ Route::get('/register',[RegisteredUserController::class, 'create']);
 Route::post('/register',[RegisteredUserController::class, 'store']);
 
 Route::get('/login', [SessionController::class, 'create']);
+Route::post('/login', [SessionController::class, 'store']);
 
 
 
