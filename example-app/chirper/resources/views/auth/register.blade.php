821e1bd01c0b9451b309d7d8c0177a7b09977d9e
ThomasTadesse <328704@student.mboutrecht.nl>
2024-10-25 13:31:55 +0200

    reg page layout and button text

--- example-app/chirper/resources/views/auth/register.blade.php
+++ example-app/chirper/resources/views/auth/register.blade.php
@@ -7,7 +7,7 @@
         @csrf
         <div class="space-y-12">
             <div class="border-b border-gray-900/10 pb-12">
-                <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                <div class="grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
                     
                     <x-form-field>
                         <x-form-label for="first_name">First name</x-form-label>
@@ -55,8 +55,8 @@
             </div>
 
             <div class="mt-6 flex items-center justify-end gap-x-6">
-                <button type="button" class="text-sm font-semibold leading-6 text-gray-900">Cancel</button>
-                <x-form-button>Save</x-form-button>
+                <a href="/home" class="text-sm font-semibold leading-6 text-gray-900">Cancel</a>
+                <x-form-button>Register</x-form-button>
             </div>
         </div>
     </form>
