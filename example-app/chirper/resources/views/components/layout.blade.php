f83ea2c48694a2b6a525645735d589f0189c254f
ThomasTadesse <328704@student.mboutrecht.nl>
2024-11-04 18:22:13 +0100

    26: Get Your Build Process in Order compleet

--- example-app/chirper/resources/views/components/layout.blade.php
+++ example-app/chirper/resources/views/components/layout.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>L11</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite(['resources/js/app.js'])
 </head>
 <body class="h-full">
 <div class="min-h-full">
