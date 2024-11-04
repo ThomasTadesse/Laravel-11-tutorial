6f99dc4229169c709d4f5ce5179d3397c91cb730
ThomasTadesse <328704@student.mboutrecht.nl>
2024-11-04 18:02:19 +0100

    add background color to body test via css

--- example-app/chirper/resources/views/components/layout.blade.php
+++ example-app/chirper/resources/views/components/layout.blade.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home Page</title>
+    <title>L11</title>
+    @vite(['resources/css/app.css'])
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="h-full">
