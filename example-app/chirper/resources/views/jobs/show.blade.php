db4d5029573ec093d19adb52b3787d3ed54c2ca5
ThomasTadesse <328704@student.mboutrecht.nl>
2024-11-04 16:18:28 +0100

    23: 6 Steps to Authorization Mastery compleet

--- example-app/chirper/resources/views/jobs/show.blade.php
+++ example-app/chirper/resources/views/jobs/show.blade.php
@@ -9,7 +9,7 @@
     </p>
 
     @can('edit', $job)
-        <p class="mt-6">
+        <p class="mt-4">
             <x-button href="/jobs/{{ $job->id }}/edit">Edit Job</x-button>
         </p>
     @endcan
