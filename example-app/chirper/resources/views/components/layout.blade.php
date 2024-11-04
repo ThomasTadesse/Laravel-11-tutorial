245ea55769fe3be697242ebb41566362a8c9e02d
ThomasTadesse <328704@student.mboutrecht.nl>
2024-11-04 18:10:18 +0100

    tailwind css refractor

--- example-app/chirper/resources/views/components/layout.blade.php
+++ example-app/chirper/resources/views/components/layout.blade.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>L11</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
-    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="h-full">
 <div class="min-h-full">
