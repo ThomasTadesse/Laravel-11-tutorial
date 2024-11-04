e2d614bf37d58db9ff9523ec29b3eceeeb761d91
ThomasTadesse <328704@student.mboutrecht.nl>
2024-11-04 13:44:17 +0100

    create.blade.php misc fixes

--- example-app/chirper/resources/views/jobs/create.blade.php
+++ example-app/chirper/resources/views/jobs/create.blade.php
@@ -3,47 +3,38 @@
         Create Job
     </x-slot:heading>
     
- 
-<form method="POST" action="/jobs">
-    @csrf
-    <!-- create the token to the form. -->
-     
-  <div class="space-y-12">
-    <div class="border-b border-gray-900/10 pb-12">
-      <h2 class="text-base font-semibold leading-7 text-gray-900">Create a new job</h2>
-      <p class="mt-1 text-sm leading-6 text-gray-600">This information will be required, So be careful what you put out there.</p>
+    <form method="POST" action="/jobs">
+        @csrf
+        <!-- create the token to the form. -->
+         
+        <div class="space-y-12">
+            <div class="border-b border-gray-900/10 pb-12">
+                <h2 class="text-base font-semibold leading-7 text-gray-900">Create a new job</h2>
+                <p class="mt-1 text-sm leading-6 text-gray-600">This information will be required, So be careful what you put out there.</p>
 
-      <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-        <x-form-field>
-          <x-form-label for="title"/>Title</x-form-label>
+                <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                    <x-form-field>
+                        <x-form-label for="title">Title</x-form-label>
+                        <div class="mt-2">
+                            <x-form-input name="title" id="title" placeholder="Software Engineer"/>
+                            <x-form-error name="title"></x-form-error>
+                        </div>
+                    </x-form-field>
 
-          <div class="mt-2">
-          <x-form-input name="title" id="title" placeholder="Software Engineer"/>
+                    <x-form-field>
+                        <x-form-label for="description">Description</x-form-label>
+                        <div class="mt-2">
+                            <x-form-input name="description" id="description" placeholder="Makes the code like a boss"/>
+                            <x-form-error name="description"></x-form-error>
+                        </div>
+                    </x-form-field>
+                </div>        
 
-          <x-form-error name="title"></x-form-error>
-
-          </div>
-        </x-form-field>
-
-
-
-        <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-        <x-form-field>
-          <x-form-label for="description"/>Description</x-form-label>
-
-          <div class="mt-2">
-          <x-form-input name="description" id="description" placeholder="Makes the code like a boss"/>
-
-          <x-form-error name="description"></x-form-error>
-
-          </div>
-        </x-form-field>        
-      
-
-  <div class="mt-6 flex items-center justify-end gap-x-6">
-    <button type="button" class="text-sm font-semibold leading-6 text-gray-900">Cancel</button>
-    <x-form-button>Save</x-form-button>
-  </div>
-</form>
-
+                <div class="mt-6 flex items-center justify-end gap-x-6">
+                    <a href="/jobs" class="text-sm font-semibold leading-6 text-gray-900">Cancel</a>
+                    <x-form-button>Save</x-form-button>
+                </div>
+            </div>
+        </div>
+    </form>
 </x-layout>
