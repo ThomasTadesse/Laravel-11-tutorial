6c939724edd53e434cf3fa35f914e324973873bc
ThomasTadesse <328704@student.mboutrecht.nl>
2024-11-04 14:07:31 +0100

    login succes

--- example-app/chirper/app/Http/Controllers/SessionController.php
+++ example-app/chirper/app/Http/Controllers/SessionController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class SessionController extends Controller
 {
@@ -13,11 +14,27 @@ class SessionController extends Controller
 
     public function store()
     {
-        dd(request()->all());
+        // validate
+        $attributes = request()->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        // attempt login
+        Auth::attempt($attributes);
+
+        // regenerate session token
+        // Happy
+        request()->session()->regenerate();
+
+        // redirect
+        return redirect('/jobs');
     }
 
     public function destroy()
     {
-        dd('logging out');
+        Auth::logout();
+
+        return redirect('/home');
     }
 }
