7c5217b816741037adae3f08117b64d327e2b8cb
ThomasTadesse <328704@student.mboutrecht.nl>
2024-11-04 17:15:37 +0100

    Queue email sending for JobPosted notification

--- example-app/chirper/app/Http/Controllers/JobController.php
+++ example-app/chirper/app/Http/Controllers/JobController.php
@@ -63,7 +63,7 @@ class JobController extends Controller
             'employer_id' => 1
         ]);
 
-        Mail::to($job->employer->user)->send(
+        Mail::to($job->employer->user)->queue(
             new JobPosted($job)
         );
         // Laravel will automatically grab the email off the user.
