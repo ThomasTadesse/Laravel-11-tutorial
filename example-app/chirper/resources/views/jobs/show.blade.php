2c7673b99031f602dbe615b14a201ad776e76fce
ThomasTadesse <328704@student.mboutrecht.nl>
2024-11-04 16:18:59 +0100

    24: How to Preview and Send Email Using Mailable Classes initial start

--- example-app/chirper/resources/views/jobs/show.blade.php
+++ example-app/chirper/resources/views/jobs/show.blade.php
@@ -9,7 +9,7 @@
     </p>
 
     @can('edit', $job)
-        <p class="mt-4">
+        <p class="mt-6">
             <x-button href="/jobs/{{ $job->id }}/edit">Edit Job</x-button>
         </p>
     @endcan
